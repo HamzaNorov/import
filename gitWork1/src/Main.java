97d11ea2d95094bf5d444765905db5f81ba2e4d8
HamzaNorov <bahtiernorov@gmail.com>
2023-03-23 20:07:43 +0300

    конвертер валют (казахский тенге)

---
+++ gitWork1/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите сумму в тенге: ");
+        double tenge = input.nextDouble();
+
+        // Курсы обмена валют на 21 марта 2023 года
+        double usdExchangeRate = 462.36;
+        double eurExchangeRate = 493.48;
+        double rubExchangeRate = 5.99;
+
+        //   тенге в доллары рубли и евро
+        double usd = tenge / usdExchangeRate;
+        double eur = tenge / eurExchangeRate;
+        double rub = tenge / rubExchangeRate;
+
+        // Вывод результатов
+        System.out.printf("%.2f тенге = %.2f долларов\n", tenge, usd);
+        System.out.printf("%.2f тенге = %.2f евро\n", tenge, eur);
+        System.out.printf("%.2f тенге = %.2f рубль\n", tenge, rub);
+    }
+}
